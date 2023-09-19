f370da3e9d2387c85618f8c6e9e3d6a4cfebaf12
cifcibilal <cifci.bilal@outlook.com>
2023-09-19 15:55:24 +0300

    d

--- ExcelReadWriteApp/Form1.Designer.cs
+++ ExcelReadWriteApp/Form1.Designer.cs
@@ -31,11 +31,6 @@
             this.openFileDialogExcel = new System.Windows.Forms.OpenFileDialog();
             this.btnSelectFile = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnSaveAs = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtFileName = new System.Windows.Forms.TextBox();
             this.txtFilePath = new System.Windows.Forms.TextBox();
@@ -51,7 +46,7 @@
             // 
             // btnSelectFile
             // 
-            this.btnSelectFile.Location = new System.Drawing.Point(317, 31);
+            this.btnSelectFile.Location = new System.Drawing.Point(319, 18);
             this.btnSelectFile.Name = "btnSelectFile";
             this.btnSelectFile.Size = new System.Drawing.Size(125, 47);
             this.btnSelectFile.TabIndex = 0;
@@ -61,75 +56,23 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.btnSaveAs);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBoxSutun);
+            this.groupBox1.Controls.Add(this.listBox1);
             this.groupBox1.Controls.Add(this.txtFileName);
             this.groupBox1.Controls.Add(this.btnSelectFile);
             this.groupBox1.Location = new System.Drawing.Point(12, 10);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(448, 475);
+            this.groupBox1.Size = new System.Drawing.Size(452, 537);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Dosya İşlemleri";
             // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(169, 282);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(120, 24);
-            this.comboBox2.TabIndex = 8;
-            this.comboBox2.Visible = false;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(10, 282);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(120, 24);
-            this.comboBox1.TabIndex = 5;
-            this.comboBox1.Visible = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(136, 282);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(27, 29);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "+";
-            this.label4.Visible = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 243);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(196, 16);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "2 Sütun Birleştir ve Farklı Kaydet";
-            this.label3.Visible = false;
-            // 
-            // btnSaveAs
-            // 
-            this.btnSaveAs.Location = new System.Drawing.Point(308, 259);
-            this.btnSaveAs.Name = "btnSaveAs";
-            this.btnSaveAs.Size = new System.Drawing.Size(125, 47);
-            this.btnSaveAs.TabIndex = 3;
-            this.btnSaveAs.Text = "Farklı Kaydet";
-            this.btnSaveAs.UseVisualStyleBackColor = true;
-            this.btnSaveAs.Visible = false;
-            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 46);
+            this.label1.Location = new System.Drawing.Point(88, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 16);
             this.label1.TabIndex = 2;
@@ -137,14 +80,14 @@
             // 
             // txtFileName
             // 
-            this.txtFileName.Location = new System.Drawing.Point(89, 43);
+            this.txtFileName.Location = new System.Drawing.Point(91, 43);
             this.txtFileName.Name = "txtFileName";
             this.txtFileName.Size = new System.Drawing.Size(222, 22);
             this.txtFileName.TabIndex = 1;
             // 
             // txtFilePath
             // 
-            this.txtFilePath.Location = new System.Drawing.Point(1058, 12);
+            this.txtFilePath.Location = new System.Drawing.Point(1012, 12);
             this.txtFilePath.Name = "txtFilePath";
             this.txtFilePath.Size = new System.Drawing.Size(10, 22);
             this.txtFilePath.TabIndex = 4;
@@ -154,7 +97,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(477, 81);
+            this.listBox1.Location = new System.Drawing.Point(6, 126);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(438, 404);
             this.listBox1.TabIndex = 3;
@@ -162,7 +105,7 @@
             // comboBoxSutun
             // 
             this.comboBoxSutun.FormattingEnabled = true;
-            this.comboBoxSutun.Location = new System.Drawing.Point(604, 50);
+            this.comboBoxSutun.Location = new System.Drawing.Point(93, 96);
             this.comboBoxSutun.Name = "comboBoxSutun";
             this.comboBoxSutun.Size = new System.Drawing.Size(196, 24);
             this.comboBoxSutun.TabIndex = 3;
@@ -171,7 +114,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(665, 31);
+            this.label2.Location = new System.Drawing.Point(4, 99);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(83, 16);
             this.label2.TabIndex = 4;
@@ -181,10 +124,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(939, 501);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBoxSutun);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(470, 554);
             this.Controls.Add(this.txtFilePath);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -207,11 +147,6 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBoxSutun;
-        private System.Windows.Forms.Button btnSaveAs;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label4;
     }
 }
 
--- ExcelReadWriteApp/Form1.cs
+++ ExcelReadWriteApp/Form1.cs
@@ -1,29 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using ExcelAcmaVeOkumaEgitim;
-using Microsoft.Office.Interop.Excel;
-using _Excel = Microsoft.Office.Interop.Excel;
-using System.Data.Odbc;
-using System.Data.OleDb;
 
 namespace ExcelReadWriteApp
 {
     public partial class Form1 : Form
     {
-        //private string[,] read = new string[12, 4];
         OpenFileDialog fileSelect;
         Excel excel;
         public Form1()
         {
             InitializeComponent();
-            
         }
 
         private void btnSelectFile_Click(object sender, EventArgs e)
@@ -44,86 +31,26 @@ namespace ExcelReadWriteApp
             excel = new Excel(txtFilePath.Text, 1);
             int satirSayisi = excel.ws.UsedRange.Rows.Count;
             int sutunSayisi = excel.ws.UsedRange.Columns.Count;
-            
+
             string[,] read = excel.ReadRange(1, 1, satirSayisi, sutunSayisi);
             excel.Close();
             //12 satır sayısını ifade ediyor
             for (int i = 1; i < satirSayisi; i++) //i 1 olmasının sebebi ilk satırlar sütun ismi
             {
                 listBox1.Items.Add("{");
-                if (i<=sutunSayisi)
+                if (i <= sutunSayisi)
                 {
                     comboBoxSutun.Items.Add(read[0, i - 1]);
-                    comboBox2.Items.Add(read[0, i - 1]);
-                    comboBox1.Items.Add(read[0, i - 1]);
-
                 }
                 for (int p = 0; p < sutunSayisi; p++)
                 {
-                    listBox1.Items.Add(read[0,p]+" : " +read[i, p]);
+                    listBox1.Items.Add(read[0, p] + " : " + read[i, p]);
                 }
                 listBox1.Items.Add("}");
                 listBox1.Items.Add("");
             }
         }
-            private void comboBoxSutun_SelectedIndexChanged(object sender, EventArgs e)
-            {
-                excel = new Excel(txtFilePath.Text, 1);
-                int satirSayisi = excel.ws.UsedRange.Rows.Count;
-                int sutunSayisi = excel.ws.UsedRange.Columns.Count;
-                string[,] read = excel.ReadRange(1, 1,satirSayisi, sutunSayisi);
-                excel.Close();
-
-            listBox1.Items.Clear();
-
-                int secilenBox = comboBoxSutun.SelectedIndex;
-            /*
-                if (secilenBox == 0) 
-                {
-                    listBox1.Items.Clear();
-                    for (int i = 0; i < satirSayisi; i++)
-                    {      
-                        listBox1.Items.Add(read[i, 0]);
-                    }
-                }
-                else if (secilenBox == 1)
-                {
-                    listBox1.Items.Clear();
-                    for (int i = 0; i < satirSayisi; i++)
-                    {
-                        listBox1.Items.Add(read[i, 1]);
-                    }
-                }
-                else if (secilenBox == 2)
-                {
-                    listBox1.Items.Clear();
-                    for (int i = 0; i < satirSayisi; i++)
-                    {
-                        listBox1.Items.Add(read[i, 2]);
-                    }
-                }
-                else if (secilenBox == 3)
-                {
-                    listBox1.Items.Clear();
-                    for (int i = 0; i < satirSayisi; i++)
-                    {
-                        listBox1.Items.Add(read[i, 3]);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Item seçerken hata oluştur");
-                }
-            */
-            for (int i = 0; i < satirSayisi; i++)
-            {
-                listBox1.Items.Add(read[i, secilenBox]);
-            }
-
-            
-            }
-
-        private void btnSaveAs_Click(object sender, EventArgs e)
+        private void comboBoxSutun_SelectedIndexChanged(object sender, EventArgs e)
         {
             excel = new Excel(txtFilePath.Text, 1);
             int satirSayisi = excel.ws.UsedRange.Rows.Count;
@@ -131,28 +58,14 @@ namespace ExcelReadWriteApp
             string[,] read = excel.ReadRange(1, 1, satirSayisi, sutunSayisi);
             excel.Close();
 
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            listBox1.Items.Clear();
 
-            saveFileDialog.Filter = "Excel Dosyaları|*.xls;*.xlsx|Tüm Dosyalar|*.*";
-            saveFileDialog.DefaultExt = "xlsx";
+            int secilenBox = comboBoxSutun.SelectedIndex;
 
-            Excel excelSaveAs = new Excel(@"C:\Users\muham\Desktop\KM.xlsx",1);
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            for (int i = 0; i < satirSayisi; i++)
             {
-                string savePath = saveFileDialog.FileName;
-
-                excelSaveAs.WriteRange(1, 1, satirSayisi, 1, read);
-                excelSaveAs.SaveAs(@savePath);
-                excelSaveAs.Close();
-
-                MessageBox.Show("Dosya başarılıyla kaydedildi.");
+                listBox1.Items.Add(read[i, secilenBox]);
             }
-            else
-            {
-                MessageBox.Show("Dosya kaydedilemedi.");
-            }
-
         }
     }
 }
